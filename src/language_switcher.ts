9d78e47c9c2f4dca7e3b929d2fe29e857667152d
mino-ri <hojo.origami@gmail.com>
2025-08-02 16:09:13 +0900

    [update] Language Switcher

--- src/language_switcher.ts
+++ src/language_switcher.ts
@@ -1,7 +1,11 @@
 let defaultLangCode = ""
-let switchingElements: HTMLElement[] = []
-let altSwitchingElements: HTMLImageElement[] = []
-let valueSwitchingElements: HTMLElement[] = []
+const switchingElements: SwitchingElement[] = []
+
+type SwitchingElement = {
+    element: HTMLElement,
+    isContentTarget: boolean,
+    targetAttributes?: Set<string>,
+}
 
 function switchLanguage(radioButton: HTMLInputElement) {
     const langCode = radioButton.value
@@ -11,48 +15,76 @@ function switchLanguage(radioButton: HTMLInputElement) {
     document.documentElement.lang = langCode
 
     const lsKey = `data-ls-${langCode}`
-    const lsAltKey = `data-ls-alt-${langCode}`
     const defaultLsKey = `data-ls-${defaultLangCode}`
-    const defaultLsAltKey = `data-ls-alt-${defaultLangCode}`
 
-    for (const element of switchingElements) {
-        element.textContent = element.getAttribute(lsKey) ?? element.getAttribute(defaultLsKey)
+    for (const { element, isContentTarget, targetAttributes } of switchingElements) {
+        if (isContentTarget) {
+            element.textContent = element.getAttribute(lsKey) ?? element.getAttribute(defaultLsKey)
+        }
+
+        if (targetAttributes) {
+            for (const attr of targetAttributes) {
+                const key = `data-ls-${attr}-${langCode}`
+                const defaultKey = `data-ls-${attr}-${defaultLangCode}`
+                element.setAttribute(attr, element.getAttribute(key) ?? element.getAttribute(defaultKey) ?? "")
+            }
+        }
+    }
+}
+
+function registerElement(element: HTMLElement) {
+    let entry: SwitchingElement | null = null
+
+    for (const attr of element.attributes) {
+        if (!attr.name.startsWith("data-ls-")) {
+            continue
+        }
+
+        entry = entry ?? {
+            element, 
+            isContentTarget: false,
+        }
+        const hyphenIndex = attr.name.indexOf("-", 8)
+        if (hyphenIndex === -1) {
+            entry.isContentTarget = true
+            element.setAttribute(`data-ls-${defaultLangCode}`, element.textContent ?? "")
+        } else {
+            const targetAttrName = attr.name.substring(8, hyphenIndex)
+            if (!entry.targetAttributes) {
+                entry.targetAttributes = new Set<string>()
+            }
+            entry.targetAttributes.add(targetAttrName)
+
+            element.setAttribute(`data-ls-${targetAttrName}-${defaultLangCode}`, element.getAttribute(targetAttrName) ?? "")
+        }
     }
 
-    for (const element of altSwitchingElements) {
-        element.alt = element.getAttribute(lsAltKey) ?? element.getAttribute(defaultLsAltKey) ?? ""
+    if (entry) {
+        switchingElements.push(entry)
     }
 }
 
 function loadLanguages() {
-    switchingElements = [...document.querySelectorAll("[data-ls-en]")].filter((e) => e instanceof HTMLElement)
-    altSwitchingElements = [...document.querySelectorAll("[data-ls-alt-en]")].filter((e) => e instanceof HTMLImageElement)
-    valueSwitchingElements = [...document.querySelectorAll("[data-ls-value-en]")].filter((e) => e instanceof HTMLElement)
     defaultLangCode = document.documentElement.lang
-    const lsKey = `data-ls-${defaultLangCode}`
-    const lsAltKey = `data-ls-alt-${defaultLangCode}`
-    const lsValueKey = `data-ls-value-${defaultLangCode}`
 
-    for (const element of switchingElements) {
-        element.setAttribute(lsKey, element.textContent ?? "")
-    }
-
-    for (const element of altSwitchingElements) {
-        element.setAttribute(lsAltKey, element.alt)
-    }
-
-    for (const element of valueSwitchingElements) {
-        element.setAttribute(lsValueKey, element.getAttribute("input") ?? "")
+    for (const element of document.querySelectorAll("*")) {
+        if (element instanceof HTMLElement) {
+            registerElement(element)
+        }
     }
 
     const languageSwitchers = [...document.querySelectorAll("input[name='language']")].filter((e) => e instanceof HTMLInputElement)
+    for (const switcher of languageSwitchers) {
+        switcher.addEventListener("click", function () { switchLanguage(this) })
+    }
+
     // ユーザーの言語をブラウザから読み取り、デフォルトに設定する
     for (const userLang of window.navigator.languages) {
         for (const switcher of languageSwitchers) {
             const langCode = switcher.value
             if (userLang.startsWith(langCode)) {
+                switcher.checked = true
                 if (langCode !== defaultLangCode) {
-                    switcher.checked = true
                     switchLanguage(switcher)
                 }
                 return
