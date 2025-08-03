6b779d5def017c72d90c12589f2f92ddbc78d202
mino-ri <hojo.origami@gmail.com>
2025-08-03 11:31:17 +0900

    [update] Color Palette

--- src/chord_graph.ts
+++ src/chord_graph.ts
@@ -2,32 +2,81 @@ import { PitchInfo, XLengthType, parsePitches } from "./pitch.js"
 import { ColorScheme, PitchSvgGenerator } from "./chord_graph_svg.js"
 import { AudioSource } from "./sound.js"
 
+type ColorPalette = {
+    back: string
+    main: string
+    sub: string
+    fill: string
+    pitchClass0: string
+    pitchClass1: string
+    pitchClass2: string
+    pitchClass3: string
+    pitchClass4: string
+    pitchClass5: string
+}
+
 let textArea: HTMLTextAreaElement
 let checkboxAutoSize: HTMLInputElement
 let checkboxAngleLog: HTMLInputElement
 let checkboxLimitYLength: HTMLInputElement
 let checkboxIgnoreOctave: HTMLInputElement
-let radioXInteger: HTMLInputElement
-let radioXOctaveReduced: HTMLInputElement
-let radioXShasavic: HTMLInputElement
+let selectXLength: HTMLSelectElement
 let previewSvg: SVGSVGElement
 let pitches: PitchInfo[] = []
 let colorScheme: ColorScheme
 let svgGenerator: PitchSvgGenerator
 let playingAudio: HTMLAudioElement | null = null
 
+const colorPalettes: ColorPalette[] = [
+    {
+        back: "#FEFAEE",
+        main: "#2B2F75",
+        sub: "#FDC6FE",
+        fill: "#FFFFFF",
+        pitchClass0: "#30D8D4",
+        pitchClass1: "#25938E",
+        pitchClass2: "#E39D11",
+        pitchClass3: "#F24E75",
+        pitchClass4: "#9664F0",
+        pitchClass5: "#39A2E9",
+    },
+    {
+        back: "#F4F4F6",
+        main: "#4C5156",
+        sub: "#AAAAAA",
+        fill: "#FFFFFF",
+        pitchClass0: "#11EDE8",
+        pitchClass1: "#339564",
+        pitchClass2: "#C3B827",
+        pitchClass3: "#DF4121",
+        pitchClass4: "#C838C8",
+        pitchClass5: "#217BDF",
+    },
+    {
+        back: "#676681",
+        main: "#FFFFFF",
+        sub: "#AAAAAA",
+        fill: "#676681",
+        pitchClass0: "#8CF2F6",
+        pitchClass1: "#96CC6A",
+        pitchClass2: "#ECD551",
+        pitchClass3: "#F08676",
+        pitchClass4: "#EB71C6",
+        pitchClass5: "#C779FF",
+    },
+]
+
 function loadMonzo(ignoreSave?: boolean) {
     const text = textArea.value
-    const xLengthType =
-        radioXOctaveReduced.checked ? XLengthType.OctaveReduced :
-            radioXShasavic.checked ? XLengthType.Shasavic : XLengthType.Integer
+    const xLengthType = selectXLength.value == "o" ? XLengthType.OctaveReduced
+        : selectXLength.value == "s" ? XLengthType.Shasavic : XLengthType.Integer
     pitches = parsePitches(text, checkboxIgnoreOctave.checked, xLengthType)
     svgGenerator.createSvg(pitches, {
         xLengthType,
         autoSize: checkboxAutoSize.checked,
         isAngleLog: checkboxAngleLog.checked,
         yLengthLimit: checkboxLimitYLength.checked,
-     })
+    })
 
     if (!ignoreSave) {
         saveToHash()
@@ -76,12 +125,12 @@ function playSound() {
     playingAudio.play()
 }
 
-function encodeHash(obj: {[key: string]: string}) {
+function encodeHash(obj: { [key: string]: string }) {
     return Object.keys(obj).map(key => `${key}=${obj[key]}`).join('&')
 }
 
 function decodeHash(hash: string) {
-    const obj: {[key: string]: string} = {}
+    const obj: { [key: string]: string } = {}
     hash.split('&').forEach((pair) => {
         const [key, value] = pair.split('=')
         if (key && value) {
@@ -97,7 +146,7 @@ function saveToHash() {
         i: (Number(checkboxIgnoreOctave.checked) |
             (Number(checkboxAngleLog.checked) << 1) |
             (Number(checkboxLimitYLength.checked) << 2)).toString(),
-        x: radioXInteger.checked ? 'i' : radioXOctaveReduced.checked ? 'o' : 's',
+        x: selectXLength.value,
     }
 
     history.replaceState(null, "", `${location.pathname}#${encodeHash(hashData)}`)
@@ -115,13 +164,7 @@ function loadFromHash() {
         checkboxLimitYLength.checked = flags % 8 >= 4
     }
     if (xType) {
-        if (xType === 'i') {
-            radioXInteger.checked = true
-        } else if (xType === 'o') {
-            radioXOctaveReduced.checked = true
-        } else if (xType === 's') {
-            radioXShasavic.checked = true
-        }
+        selectXLength.value = xType
     }
 }
 
@@ -130,20 +173,49 @@ function addEventListnerById<T extends HTMLElement>(id: string, eventName: strin
     target.addEventListener(eventName, () => listner(target))
 }
 
+function changeColorPalette(select: HTMLSelectElement) {
+    const palette = colorPalettes[Number(select.value)]
+    if (!palette) {
+        return
+    }
+
+    document.getElementById("color_back")?.setAttribute("value", palette.back)
+    document.getElementById("color_main")?.setAttribute("value", palette.main)
+    document.getElementById("color_sub")?.setAttribute("value", palette.sub)
+    document.getElementById("color_fill")?.setAttribute("value", palette.fill)
+    document.getElementById("color0")?.setAttribute("value", palette.pitchClass0)
+    document.getElementById("color1")?.setAttribute("value", palette.pitchClass1)
+    document.getElementById("color2")?.setAttribute("value", palette.pitchClass2)
+    document.getElementById("color3")?.setAttribute("value", palette.pitchClass3)
+    document.getElementById("color4")?.setAttribute("value", palette.pitchClass4)
+    document.getElementById("color5")?.setAttribute("value", palette.pitchClass5)
+
+    previewSvg.style.background = palette.back
+    colorScheme.noteStroke = palette.main
+    colorScheme.gridStroke = palette.sub
+    colorScheme.noteFill = palette.fill
+    colorScheme.setPitchClassColor(0, palette.pitchClass0)
+    colorScheme.setPitchClassColor(1, palette.pitchClass1)
+    colorScheme.setPitchClassColor(2, palette.pitchClass2)
+    colorScheme.setPitchClassColor(3, palette.pitchClass3)
+    colorScheme.setPitchClassColor(4, palette.pitchClass4)
+    colorScheme.setPitchClassColor(5, palette.pitchClass5)
+
+    loadMonzo()
+}
+
 window.addEventListener("load", () => {
     textArea = document.getElementById("textarea_editor") as HTMLTextAreaElement
     checkboxAutoSize = document.getElementById("checkbox_auto_size") as HTMLInputElement
     checkboxAngleLog = document.getElementById("checkbox_angle_log") as HTMLInputElement
     checkboxLimitYLength = document.getElementById("limit_y_length") as HTMLInputElement
     checkboxIgnoreOctave = document.getElementById("checkbox_ignore_octave") as HTMLInputElement
-    radioXInteger = document.getElementById("radio_x_integer") as HTMLInputElement
-    radioXOctaveReduced = document.getElementById("radio_x_octave_reduced") as HTMLInputElement
-    radioXShasavic = document.getElementById("radio_x_shasavic") as HTMLInputElement
+    selectXLength = document.getElementById("selecd_x_length") as HTMLSelectElement
     previewSvg = document.getElementById("preview_figure") as unknown as SVGSVGElement
     const grid = previewSvg.getElementById("grid") as SVGGElement
     const lineGroup = previewSvg.getElementById("line_group") as SVGGElement
     const pitchClassGroup = previewSvg.getElementById("pitch_class_group") as SVGGElement
-    
+
     colorScheme = new ColorScheme()
     svgGenerator = new PitchSvgGenerator(previewSvg, grid, lineGroup, pitchClassGroup, colorScheme)
 
@@ -153,17 +225,16 @@ window.addEventListener("load", () => {
         checkboxAngleLog,
         checkboxLimitYLength,
         checkboxIgnoreOctave,
-        radioXInteger,
-        radioXOctaveReduced,
-        radioXShasavic,
+        selectXLength,
     ]
     for (const element of uiElements) {
         element.addEventListener("input", () => loadMonzo())
     }
-
+    
     addEventListnerById("a_download_svg", "click", downloadSvg)
     addEventListnerById("a_download_png", "click", downloadPng)
     addEventListnerById("button_play", "click", playSound)
+    addEventListnerById<HTMLInputElement>("color_back", "input", (input) => { previewSvg.style.background = input.value })
     addEventListnerById<HTMLInputElement>("color_main", "input", (input) => { colorScheme.noteStroke = input.value; loadMonzo() })
     addEventListnerById<HTMLInputElement>("color_sub", "input", (input) => { colorScheme.gridStroke = input.value; loadMonzo() })
     addEventListnerById<HTMLInputElement>("color_fill", "input", (input) => { colorScheme.noteFill = input.value; loadMonzo() })
@@ -173,7 +244,8 @@ window.addEventListener("load", () => {
     addEventListnerById<HTMLInputElement>("color3", "input", (input) => { colorScheme.setPitchClassColor(3, input.value); loadMonzo() })
     addEventListnerById<HTMLInputElement>("color4", "input", (input) => { colorScheme.setPitchClassColor(4, input.value); loadMonzo() })
     addEventListnerById<HTMLInputElement>("color5", "input", (input) => { colorScheme.setPitchClassColor(5, input.value); loadMonzo() })
-    
+    addEventListnerById<HTMLSelectElement>("select_color_palette", "input", changeColorPalette)
+
     loadFromHash()
     loadMonzo(true)
 })
